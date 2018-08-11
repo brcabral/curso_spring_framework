9771f32c9ce948186759496c1a152c60f4ba14f8
Breno Cabral <brcabral@gmail.com>
2018-08-11 00:11:30 -0300

    Fim do capítulo 15 (Pesquisa, redimensionamento da foto, paginação e ordenação)

--- src/main/java/com/algaworks/brewer/controller/page/PageWrapper.java
+++ src/main/java/com/algaworks/brewer/controller/page/PageWrapper.java
@@ -5,7 +5,8 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.data.domain.Page;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
 import org.springframework.web.util.UriComponentsBuilder;
 
 public class PageWrapper<T> {
@@ -14,7 +15,10 @@ public class PageWrapper<T> {
 
 	public PageWrapper(Page<T> page, HttpServletRequest httpServletRequest) {
 		this.page = page;
-		this.uriBuilder = ServletUriComponentsBuilder.fromRequest(httpServletRequest);
+		String httpUrl = httpServletRequest.getRequestURL()
+				.append(httpServletRequest.getQueryString() != null ? "?" + httpServletRequest.getQueryString() : "")
+				.toString().replaceAll("\\+", "%20");
+		this.uriBuilder = UriComponentsBuilder.fromHttpUrl(httpUrl);
 	}
 
 	public List<T> getConteudo() {
@@ -43,5 +47,39 @@ public class PageWrapper<T> {
 
 	public String urlParaPagina(int pagina) {
 		return uriBuilder.replaceQueryParam("page", pagina).build(true).encode().toUriString();
+	}
+
+	public String urlOrdenada(String propriedade) {
+		UriComponentsBuilder uriBuilderOrder = UriComponentsBuilder
+				.fromUriString(uriBuilder.build(true).encode().toString());
+
+		String valorOrdenacao = String.format("%s,%s", propriedade, inverterOrdenacao(propriedade));
+
+		return uriBuilderOrder.replaceQueryParam("sort", valorOrdenacao).build(true).encode().toUriString();
+	}
+
+	public String inverterOrdenacao(String propriedade) {
+		String ordenacao = "asc";
+
+		Order order = page.getSort() != null ? page.getSort().getOrderFor(propriedade) : null;
+		if (order != null) {
+			ordenacao = Sort.Direction.ASC.equals(order.getDirection()) ? "desc" : "asc";
+		}
+
+		return ordenacao;
+	}
+
+	public boolean descendente(String propriedade) {
+		return inverterOrdenacao(propriedade).equals("asc");
+	}
+
+	public boolean ordenada(String propriedade) {
+		Order order = page.getSort() != null ? page.getSort().getOrderFor(propriedade) : null;
+
+		if (order == null) {
+			return false;
+		}
+
+		return page.getSort().getOrderFor(propriedade) != null ? true : false;
 	}
 }
